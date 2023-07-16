128e17d731c7adf4819defc7a7387dfc9201dd09
Herman Ceaser <ceaserbanks09@gmail.com>
2023-07-17 01:06:17 +0300

    added footer

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import EmailOutline from "vue-material-design-icons/EmailOutline.vue";
+import Github from "vue-material-design-icons/Github.vue";
+import Linkedin from "vue-material-design-icons/Linkedin.vue";
+import Twitter from "vue-material-design-icons/Twitter.vue";
+import siteMetadata from '@/Data/siteMetadata'
+
+const size = 6;
+</script>
+
+<template>
+    <footer>
+        <div class="mt-16 flex flex-col items-center">
+            <div class="mb-3 flex space-x-4">
+                <a class="text-sm text-gray-500 transition hover:text-gray-600" target="_blank"
+                    rel="noopener noreferrer" :href="`mailto:${siteMetadata.email}`">
+                    <span class="sr-only">Email</span>
+                    <email-outline
+                        class="fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400"
+                        :class="`h-${size} w-${size}`" />
+                </a>
+                <a class="text-sm text-gray-500 transition hover:text-gray-600" target="_blank"
+                    rel="noopener noreferrer" :href="siteMetadata.github">
+                    <span class="sr-only">Github</span>
+                    <Github
+                        class="fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400"
+                        :class="`h-${size} w-${size}`" />
+                </a>
+
+                <a class="text-sm text-gray-500 transition hover:text-gray-600" target="_blank"
+                    rel="noopener noreferrer" :href="siteMetadata.linkedin">
+                    <span class="sr-only">LinkedIn</span>
+                    <Linkedin
+                        class="fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400"
+                        :class="`h-${size} w-${size}`"/>
+                </a>
+                <a class="text-sm text-gray-500 transition hover:text-gray-600" target="_blank"
+                    rel="noopener noreferrer" :href="siteMetadata.twitter">
+                    <span class="sr-only">Twitter</span>
+                    <Twitter
+                        class="fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400"
+                        :class="`h-${size} w-${size}`"/>
+                </a>
+            </div>
+            <div class="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+                <div>{{siteMetadata.author}}</div>
+                <div>{{` • `}}</div>
+                <div>{{`© ${new Date().getFullYear()}`}}</div>
+                <div>{{` • `}}</div>
+                <Link href="/">{{siteMetadata.title}}</Link>
+            </div>
+            <div class="mb-8 text-sm text-gray-500 dark:text-gray-400">
+                <Link :href="siteMetadata.siteRepo">
+                LaraBlog - Built with Love using Laravel, Vue.Js and Tailwind CSS
+                </Link>
+            </div>
+        </div>
+    </footer>
+</template>
--- resources/js/Layouts/MainNavLayout.vue
+++ resources/js/Layouts/MainNavLayout.vue
@@ -1,18 +1,12 @@
 <script setup>
 import { Link, usePage } from "@inertiajs/vue3";
-import SectionContainer from "@/Components/SectionContainer.vue";
-import ApplicationLogo from "@/Components/ApplicationLogo.vue";
-import WeatherNight from "vue-material-design-icons/WeatherNight.vue";
-import LightbulbOnOutline from "vue-material-design-icons/LightbulbOnOutline.vue";
-
 import { useMainStore } from "@/Stores/main";
 import { storeToRefs } from "pinia";
-import { useDark, useToggle } from "@vueuse/core";
-import MobileNav from "@/Components/MobileNav.vue";
-import navLinks from "@/navLinks";
 
-const isDark = useDark();
-const toggleDark = useToggle(isDark);
+import SectionContainer from "@/Components/SectionContainer.vue";
+import Header from "@/Components/Header.vue";
+import Footer from "@/Components/Footer.vue";
+
 
 const useMain = useMainStore();
 const { isPostOverlay, isCropperModal, isImageDisplay } = storeToRefs(useMain);
@@ -23,52 +17,15 @@ const user = usePage().props.auth.user;
 
 <template>
     <SectionContainer>
-        <div class="flex flex-col justify-between">
-            <header id="mainNav" class="flex items-center justify-between py-10">
-                <!-- Application Logo  -->
-                <Link href="/">
-                <div class="flex items-center justify-between">
-                    <div class="mr-3">
-                        <ApplicationLogo :fillColor="isDark ? '#FFFFF4' : 'currentColor'" height="24" width="24" />
-                    </div>
-                    <div class="hidden h-6 text-2xl font-semibold sm:block">
-                        Logo
-                    </div>
-                </div>
-                </Link>
-                <!-- End Application Logo  -->
-                <div className="flex items-center text-base leading-5">
-                    <!-- Main Navigation Menu -->
-                    <div v-for="link in navLinks" :key="link.title" className="hidden sm:block">
-                        <Link :href="link.href" class="p-1 font-medium text-gray-600 dark:text-gray-100 sm:p-4">
-                        {{ link.title }}
-                        </Link>
-
-                    </div>
-                    <!-- End Main Navigation Menu  -->
-
-                    <!-- Theme Switcher -->
-                    <div id="switchTheme">
-                        <button @click="toggleDark()" v-if="isDark"
-                            class="rounded-full bg-gray-100 p-2 hover:bg-gray-300 dark:hover:bg-white:300 dark:hover:text-gray-900 mx-1 cursor-pointer">
-                            <weather-night :size="24" fillColor="#050505" />
-                        </button>
-                        <button @click="toggleDark()" v-else="isDark"
-                            class="rounded-full bg-[#E3E4EA] p-2 hover:bg-gray-300 mx-1 cursor-pointer">
-                            <lightbulb-on-outline :size="24" fillColor="currentColor" />
-                        </button>
-                    </div>
-                    <!-- End theme switcher🐱 -->
-
-                    <!-- Mobile Nav Menu  -->
-                    <MobileNav />
-                    <!-- End obile Nav Menu  -->
-                </div>
-            </header>
+        <div class="flex h-screen flex-col justify-between">
+            <Header />
 
             <main class="mb-auto">
                 <slot />
             </main>
+
+            <Footer />
+
         </div>
     </SectionContainer>
 </template>
